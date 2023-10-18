487737c2cca1a086e183daa6cc5b197736770b53
Lionheart250 <jacksonlionheart@gmail.com>
2023-10-18 13:53:37 -0700

    walking/jumping/camera following player

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,9 +7,38 @@ public class PlayerController : MonoBehaviour
     [Header("Horizontal Movement Settings:")]
     [SerializeField] private float walkSpeed = 1; //sets the players movement speed on the ground
     [Space(5)]
+
+    [Header("Vertical Movement Settings")]
+    [SerializeField] private float jumpForce = 45f; 
+    [Space(5)]
+
+    [Header("Ground Check Settings:")]
+    [SerializeField] private Transform groundCheckPoint; //point at which ground check happens
+    [SerializeField] private float groundCheckY = 0.2f; //how far down from ground chekc point is Grounded() checked
+    [SerializeField] private float groundCheckX = 0.5f; //how far horizontally from ground chekc point to the edge of the player is
+    [SerializeField] private LayerMask whatIsGround; //sets the ground layer
+    [Space(5)]
+
     [HideInInspector] public Rigidbody2D rb;
 
     private float xAxis, yAxis;
+    Animator anim;
+
+    public static PlayerController Instance;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+        
+        DontDestroyOnLoad(gameObject);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +52,8 @@ public class PlayerController : MonoBehaviour
     {
          GetInputs();
          Move();
+         Jump();
+         Flip();
     }
 
     void GetInputs()
@@ -31,9 +62,51 @@ public class PlayerController : MonoBehaviour
       yAxis = Input.GetAxisRaw("Vertical");
     }
 
+    void Flip()
+    {
+        if (xAxis < 0)
+        {
+            transform.localScale = new Vector2(-1, transform.localScale.y);
+            
+        }
+        else if (xAxis > 0)
+        {
+            transform.localScale = new Vector2(1, transform.localScale.y);
+           
+        }
+    }
+
     private void Move()
     {
         rb.velocity = new Vector2(walkSpeed * xAxis, rb.velocity.y);
         //anim.SetBool("Walking", rb.velocity.x != 0 && Grounded());
     }
+
+    public bool Grounded()
+    {
+        if (Physics2D.Raycast(groundCheckPoint.position, Vector2.down, groundCheckY, whatIsGround) 
+            || Physics2D.Raycast(groundCheckPoint.position + new Vector3(groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround) 
+            || Physics2D.Raycast(groundCheckPoint.position + new Vector3(-groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+     void Jump()
+    {
+        if(Input.GetButtonUp("Jump") && rb.velocity.y > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+        }
+
+        if (Input.GetButtonDown("Jump") && Grounded())
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce);
+        }
+    }
+    
 }
