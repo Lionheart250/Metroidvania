9241e8f262b5e1c3c149d1f5185ea76e76a1704c
Lionheart250 <jacksonlionheart@gmail.com>
2023-10-24 19:49:18 -0700

    SCENE TRANSITIONS AND CAMERA SETTINGS

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -218,6 +218,7 @@ public class PlayerController : MonoBehaviour
          GetInputs();
          UpdateJumpVariables();
          RestoreTimeScale();
+         UpdateCameraYDampForPlayerFall();
          if (pState.dashing) return;
          Flip();
          Move();
@@ -277,6 +278,23 @@ public class PlayerController : MonoBehaviour
     {
         rb.velocity = new Vector2(walkSpeed * xAxis, rb.velocity.y);
         anim.SetBool("Walking", rb.velocity.x != 0 && Grounded());
+    }
+
+    void UpdateCameraYDampForPlayerFall()
+    {
+        //if falling past a certain speed threshold
+        if (rb.velocity.y < playerFallSpeedThreshold && !CameraManager.Instance.isLerpingYDamping && !CameraManager.Instance.hasLerpedYDamping)
+        {
+            StartCoroutine(CameraManager.Instance.LerpYDamping(true));
+        }
+        //if standing stil or moving up
+        if(rb.velocity.y >= 0 && !CameraManager.Instance.isLerpingYDamping && CameraManager.Instance.hasLerpedYDamping)
+        {
+            //reset camera function
+            CameraManager.Instance.hasLerpedYDamping = false;
+            StartCoroutine(CameraManager.Instance.LerpYDamping(false));
+        }
+
     }
 
     void StartDash()
